51f3e04f390afd512e1c985a2fa62dd823951b13
Suyash2008 <72138962+Suyash2008@users.noreply.github.com>
2020-12-14 17:48:48 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,28 @@
+class Chain{
+    constructor(bodyA,pointB)
+    {
+        var option = {
+            bodyA:bodyA,pointB:pointB,
+            stiffness : 0.04,
+            length : 5
+
+        }
+        this.pointB = pointB
+        this.chain = Matter.Constraint.create(option)
+        World.add(world,this.chain);
+    }
+    display(){
+        if(this.chain.bodyA){
+        var pa = this.chain.bodyA.position
+        var pb =  this.pointB
+        stroke("brown");
+        line(pa.x,pa.y,pb.x,pb.y)
+        }
+    }
+    fly(){
+        this.chain.bodyA = null;
+    }
+    attach(body){
+      this.chain.bodyA = body;  
+    }
+}
